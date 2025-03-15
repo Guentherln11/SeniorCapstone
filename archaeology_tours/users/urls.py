2d62700a4ff23472fef2629b701999f197cb7f7e
Guentherln11 <neonbrownies1337@gmail.com>
2025-03-15 17:00:58 -0400

    Added basic user registration (/users/register/)

--- archaeology_tours/users/urls.py
+++ archaeology_tours/users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'berry'
-
 urlpatterns = [
-    path("", views.index, name="index")
+    path("register/", views.register, name="register")
 ]
