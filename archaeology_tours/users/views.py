a55a333e3ce953b7a508a6c5530c7f9979083df0
Guentherln11 <neonbrownies1337@gmail.com>
2025-03-25 14:08:34 -0400

    Created basic profile page, started on database integration

--- archaeology_tours/users/views.py
+++ archaeology_tours/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -18,8 +19,11 @@ def login_p(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect("/berry/")
+            return redirect("/users/profile/")
 
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", { "form": form })
+
+def profile(request):
+    return render(request, "users/profile.html")
