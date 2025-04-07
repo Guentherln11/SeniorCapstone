2866b677960dc49ead83c9635ff2febaf600ec94
Guentherln11 <neonbrownies1337@gmail.com>
2025-04-07 17:43:13 -0400

    Made dynamic videos page

--- archaeology_tours/berry/admin.py
+++ archaeology_tours/berry/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Annotations, bImages
+from mysite.models import Videos
 
 # Register your models here.
 admin.site.register(Annotations)
 admin.site.register(bImages)
+admin.site.register(Videos)
