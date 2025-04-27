d206723fd207cc85f47d5beb04f127f3b20ade62
guentherln11 <neonbrownies1337@gmail.com>
2025-04-27 19:42:21 +0000

    Added ability to see stamp collectors in admin panel

--- archaeology_tours/berry/admin.py
+++ archaeology_tours/berry/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from .models import Annotations, bImages, Additional, Question, Answer, Stamp
 from mysite.models import Videos
-
+from users.models import UserStamp
 # Register your models here.
 admin.site.register(Annotations)
 admin.site.register(bImages)
 admin.site.register(Additional)
 admin.site.register(Videos)
-admin.site.register(Stamp)
 class AnswerInline(admin.TabularInline):
     model = Answer
     extra = 4
@@ -16,3 +15,14 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
 
 admin.site.register(Question, QuestionAdmin)
+
+class UserStampInline(admin.TabularInline):
+    model = UserStamp
+    extra = 0
+    readonly_fields = ('profile',)
+
+class StampAdmin(admin.ModelAdmin):
+    inlines = [UserStampInline]
+
+admin.site.register(Stamp, StampAdmin)
+admin.site.register(UserStamp)
